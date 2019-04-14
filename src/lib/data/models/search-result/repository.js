5e5512d011083e869f4d4a3c86c4404ca3847bdf
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-04-14 15:26:52 +0900

    return promises from repository methods
    in future, a real db will return results within a promise so for
    smoother transition lets follow that api from the begining

--- src/lib/data/models/search-result/repository.js
+++ src/lib/data/models/search-result/repository.js
@@ -1,7 +1,11 @@
 const db = [];
 
-export const find = ({ word, url }) => (
-  db.filter(s => s.word === word && s.url === url)[0] || null
-);
+export const find = ({ word, url }) => new Promise((resolve, _reject) => {
+  const result = db.filter(s => s.word === word && s.url === url)[0] || null;
+  resolve(result);
+});
 
-export const add = searchResult => db.push(searchResult);
+export const add = searchResult => new Promise((resolve, _reject) => {
+  const result = db.push(searchResult);
+  resolve(result);
+});
--- src/lib/data/models/search-result/spec.js
+++ src/lib/data/models/search-result/spec.js
@@ -8,22 +8,22 @@ describe('SearchResultRepository', () => {
     count: 9001,
   });
 
-  it('adds a model to db', () => {
-    add(searchResult);
+  it('adds a model to db', async () => {
+    await add(searchResult);
 
-    const wasModelAdded = !!find({ word: 'javascript', url: 'http://www.jakubniewczas.com/' });
+    const wasModelAdded = await !!find({ word: 'javascript', url: 'http://www.jakubniewczas.com/' });
     expect(wasModelAdded).to.be.true();
   });
 
-  it('finds a model', () => {
-    add(searchResult);
+  it('finds a model', async () => {
+    await add(searchResult);
 
-    const findResult = find({ word: 'javascript', url: 'http://www.jakubniewczas.com/' });
+    const findResult = await find({ word: 'javascript', url: 'http://www.jakubniewczas.com/' });
     expect(findResult.count).to.be.eq(9001);
   });
 
-  it('returns null when a model could not be found', () => {
-    const result404 = find({ word: '404', url: '4-4' });
+  it('returns null when a model could not be found', async () => {
+    const result404 = await find({ word: '404', url: '4-4' });
     expect(result404).to.be.eq(null);
   });
 });
